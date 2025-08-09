e7f40258eb9eec6fde9a23bf34db98c0ce873307
Jessemm00 <jessmsemm@gmail.com>
2025-08-09 13:58:34 +1000

    backend set up

---
+++ Backend/src/server.js
@@ -0,0 +1,16 @@
+// backend/src/server.js
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import signupRoute from "./routes/signup.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", signupRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

f1748ac94330575f63d27fe787923b848d73d88f
matthewgkc <matthewgkcollis@gmail.com>
2025-08-09 13:28:06 +1000

    Added Navbar component with responsive sizing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="app">
+      <Navbar />
+
+      {/* Landing Content */}
+      <main className="main-content">
         <h1>Welcome to AA Coders web app!!</h1>
-        <p>
-          This is a simple template to get you started with Vite and React.
-        </p>
-    </>
+        <p>This is a simple template to get you started with Vite and React.</p>
+      </main>
+    </div>
   )
 }
 
